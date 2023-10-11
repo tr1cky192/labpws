ae8f920fd6be5ddfff2f5212b6b72c020d96a9c7
tr1cky192 <106375248+tr1cky192@users.noreply.github.com>
2023-10-11 15:20:11 +0300

    Delete lab2 directory

--- lab2/project02.02/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-await app.StartAsync();
-await Task.Delay(10000);
-await app.StopAsync();
