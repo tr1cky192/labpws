eaad70ad208738e50b7c0ebf9b3a77d4a1d0aed7
tr1cky192 <106375248+tr1cky192@users.noreply.github.com>
2023-10-11 15:22:21 +0300

    Add files via upload

---
+++ lab2/lab2/Startup.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            int x = 5;
+            int y = 8;
+            int z = 0;
+            app.Use(async (context, next) =>
+            {
+                z = x * y;
+                await next.Invoke();
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"x * y = {z}");
+            });
+        }
+    }
+}
