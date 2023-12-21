ffbe7c3b38236cfd6aba0dd224228d121399c20a
tr1cky192 <106375248+tr1cky192@users.noreply.github.com>
2023-12-21 18:45:49 +0200

    Add files via upload

---
+++ 8/project02.08/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.Run(async (context) =>
+{
+    context.Response.StatusCode = 404;
+    await context.Response.WriteAsync("UNLUCKY");
+});
+
+app.Run();
