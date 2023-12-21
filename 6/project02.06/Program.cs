bc68fb751d6d4726882e34d4e23a55bd32512c07
tr1cky192 <106375248+tr1cky192@users.noreply.github.com>
2023-12-21 18:44:33 +0200

    Add files via upload

---
+++ 6/project02.06/Program.cs
@@ -0,0 +1,11 @@
+﻿var builder = WebApplication.CreateBuilder();
+
+var app = builder.Build();
+
+int x = 16;
+app.Run(async (context) =>
+{
+    x = x / 4; 
+    await context.Response.WriteAsync($"Result: {x}");
+});
+app.Run();
