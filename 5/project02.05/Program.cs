a19bbb3bfaf524f2552ae62a135847182d7957ba
tr1cky192 <106375248+tr1cky192@users.noreply.github.com>
2023-12-21 18:34:56 +0200

    Add files via upload

---
+++ 5/project02.05/Program.cs
@@ -0,0 +1,11 @@
+﻿var builder = WebApplication.CreateBuilder();
+
+var app = builder.Build();
+
+app.Run(HandleRequst);
+app.Run();
+
+async Task HandleRequst(HttpContext context)
+{
+    await context.Response.WriteAsync("DOTA 2, IMORTAL");
+}
